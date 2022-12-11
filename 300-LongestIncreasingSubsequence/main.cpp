5f29e36cded6effb7898992ae280ffec4a43e04b
tian-shu <tian-shu@sjtu.edu.cn>
2022-12-11 09:48:07 -0500

    LIS1

--- 300-LongestIncreasingSubsequence/main.cpp
+++ 300-LongestIncreasingSubsequence/main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 
 int LIS (vector<int> & nums) {
+    vector<size_t> memo(nums.size());
+    memo[0] = 1;
+    for (size_t i = 1; i < memo.size(); i++) {
+        size_t currentMax = 0;
+        for (size_t j = 0; j < i; j++) {
+            if (nums[i] > nums[j] && memo[j] + 1 > currentMax) {
+                currentMax = memo[j] + 1;
+            }
+        }
+        memo[i] = currentMax;
+    }
     
 }
 
