a9351ac396b40417670023077377b118002ff27c
tian-shu <tian-shu@sjtu.edu.cn>
2022-12-21 17:34:49 -0500

    SetMatrix

--- MediumInterview-SetMatrixZeroes/main.cpp
+++ MediumInterview-SetMatrixZeroes/main.cpp
@@ -3,35 +3,78 @@
 using namespace std;
 
 
+// void setZeroes(vector<vector<int>> & matrix) {
+//     for (size_t i = 0; i < matrix.size(); i++) {
+//         for (size_t j = 0; j < matrix[0].size(); j++) {
+//             if (matrix[i][j] != 0) {
+//                 matrix[i][j] = 1;
+//             }
+//         }
+//     }
+//     for (size_t i = 0; i < matrix.size(); i++) {
+//         for (size_t j = 0; j < matrix[0].size(); j++) {
+//             if (matrix[i][j] == 0) {
+//                 for (size_t k = 0; k < matrix[0].size(); k++) {
+//                     if (matrix[i][k] == 1) {
+//                         matrix[i][k] = -1;
+//                     }
+//                 }
+//                 for (size_t k = 0; k < matrix.size(); k++) {
+//                     if (matrix[k][j] == 1) {
+//                         matrix[k][j] = -1;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     for (size_t i = 0; i < matrix.size(); i++) {
+//         for (size_t j = 0; j < matrix[0].size(); j++) {
+//             if (matrix[i][j] == -1) {
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
+// }
+
+
 void setZeroes(vector<vector<int>> & matrix) {
-    for (size_t i = 0; i < matrix.size(); i++) {
-        for (size_t j = 0; j < matrix[0].size(); j++) {
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = 1;
-            }
-        }
-    }
+    bool R1 = false, C1 = false;
     for (size_t i = 0; i < matrix.size(); i++) {
         for (size_t j = 0; j < matrix[0].size(); j++) {
             if (matrix[i][j] == 0) {
-                for (size_t k = 0; k < matrix[0].size(); k++) {
-                    if (matrix[i][k] == 1) {
-                        matrix[i][k] = -1;
-                    }
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+                if (i == 0) {
+                    R1 = true;
                 }
-                for (size_t k = 0; k < matrix.size(); k++) {
-                    if (matrix[k][j] == 1) {
-                        matrix[k][j] = -1;
-                    }
+                if (j == 0) {
+                    C1 = true;
                 }
             }
         }
     }
-    for (size_t i = 0; i < matrix.size(); i++) {
-        for (size_t j = 0; j < matrix[0].size(); j++) {
-            if (matrix[i][j] == -1) {
+    for (size_t i = 1; i < matrix.size(); i++) {
+        if (matrix[i][0] == 0) {
+            for (size_t j = 0; j < matrix[0].size(); j++) {
                 matrix[i][j] = 0;
             }
+        }
+    }
+    for (size_t j = 1; j < matrix[0].size(); j++) {
+        if (matrix[0][j] == 0) {
+            for (size_t i = 0; i < matrix.size(); i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    if (C1) {
+        for (size_t i = 1; i < matrix.size(); i++) {
+            matrix[i][0] = 0;
+        }
+    }
+    if (R1) {
+        for (size_t j = 1; j < matrix[0].size(); j++) {
+            matrix[0][j] = 0;
         }
     }
 }
