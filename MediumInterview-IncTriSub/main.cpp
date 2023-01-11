b4c6625a479c0bebd8afadf9d3c56cb49de2f51c
tian-shu <tian-shu@sjtu.edu.cn>
2023-01-11 17:00:17 -0500

    increasing triplet

--- MediumInterview-IncTriSub/main.cpp
+++ MediumInterview-IncTriSub/main.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+#include <limits>
+#include <vector>
+using namespace std;
+
+
+bool incTriplet (vector<int> & nums) {
+    if (nums.size() < 3) {
+        return false;
+    }
+    int a = numeric_limits<int>::max();
+    int b = numeric_limits<int>::max();
+    for (auto i : nums) {
+        a = min(a, i);
+        if (i > b) {
+            return true;
+        }
+        if (i > a) {
+            b = i;
+        }
+    }
+    return false;
+}
 
 
 int main () {
