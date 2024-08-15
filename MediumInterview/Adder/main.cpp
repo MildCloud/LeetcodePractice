ecb53fab2bc984d727cac85b52a3c22f3afe2ff6
Tianshu <tian-shu@sjtu.edu.cn>
2024-08-15 17:59:18 +0800

    my adder tst

--- MediumInterview/Adder/main.cpp
+++ MediumInterview/Adder/main.cpp
@@ -1,9 +1,20 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
 int getSum(int a, int b) {
     int c; 
     while(b !=0 ) {
-        c = (a&b);
+        cout << bitset<32>(a) << endl;
+        cout << bitset<32>(b) << endl;
+        c = a&b;
         a = a ^ b;
-        b = (c)<<1;
+        b = c<<1;
     }
     return a;
+}
+
+int main() {
+    cout << getSum(15, 14) << endl;
+    return 0;
 }
