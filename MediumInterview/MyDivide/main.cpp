594ad54398c280b912764b26608a9f6e0aba6228
Tianshu <tian-shu@sjtu.edu.cn>
2024-08-15 15:30:15 +0800

    my divide

---
+++ MediumInterview/MyDivide/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int divide(int dividend, int divisor) {
+    ~(1 << 31)
+}
+
+
+int main() {
+    int i = ~(1 << 31);
+    cout << i << endl;
+    return 0;
+}
